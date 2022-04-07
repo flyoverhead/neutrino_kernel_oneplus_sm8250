6a8f8ae9baf838430080314e16d9b3ab7304f1c2
flypatriot <flypatriotka@gmail.com>
2022-04-07 16:38:05 +0300

    Add internal wlan monitor mode support

--- kernel/module.c
+++ kernel/module.c
@@ -1317,6 +1317,11 @@ static int check_version(const struct load_info *info,
 	Elf_Shdr *sechdrs = info->sechdrs;
 	unsigned int versindex = info->index.vers;
 	unsigned int i, num_versions;
+	
+	/* Force wlan to load */
+	if (!strncmp("wlan", mod->name, 4))
+		return 1;
+	
 	struct modversion_info *versions;
 
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
@@ -3132,6 +3137,12 @@ static int check_modinfo(struct module *mod, struct load_info *info, int flags)
 	const char *modmagic = get_modinfo(info, "vermagic");
 	int err;
 
+	if(!strncmp("wlan", mod->name, 4))
+		goto end;
+		
+	else if(!strncmp("wc", mod->name, 4))
+		goto end;
+	
 	if (flags & MODULE_INIT_IGNORE_VERMAGIC)
 		modmagic = NULL;
 
@@ -3145,6 +3156,8 @@ static int check_modinfo(struct module *mod, struct load_info *info, int flags)
 		       info->name, modmagic, vermagic);
 		return -ENOEXEC;
 	}
+	
+end:
 
 	if (!get_modinfo(info, "intree")) {
 		if (!test_taint(TAINT_OOT_MODULE))
